f313b4654aa1703f4fd41aacb27d28d879da0990
Rohit Sharma <rohits8731@gmail.com>
2025-01-30 00:14:26 +0530

    final commit

---
+++ sentry.server.config.ts
@@ -0,0 +1,12 @@
+// This file configures the initialization of Sentry on the server.
+// The config you add here will be used whenever the server handles a request.
+// https://docs.sentry.io/platforms/javascript/guides/nextjs/
+
+import * as Sentry from "@sentry/nextjs";
+
+Sentry.init({
+  dsn: "https://7ffe7ca827ae47ffe3fbc0fc9c388cd3@o4507666684641280.ingest.us.sentry.io/4508727595761664",
+
+  // Setting this option to true will print useful information to the console while you're setting up Sentry.
+  debug: false,
+});
